dfafe893fd084751fcf6deafc40996ed729a35e0
kumar <obulesu.1406@gmail.com>
2025-04-11 11:39:36 -0400

    updated setup

--- ci-cd-tools/jenkins.sh
+++ ci-cd-tools/jenkins.sh
@@ -1,17 +1,3 @@
-# #!/bin/bash
-# #Included 2 below commands for kubernetes configuration but these are general to any containerization
-# #begins
-# ARCH=amd64
-# PLATFORM=$(uname -s)_$ARCH
-# #ends
-
-# growpart /dev/nvme0n1 4
-# lvextend -l +50%FREE /dev/RootVG/rootVol
-# lvextend -l +50%FREE /dev/RootVG/varVol
-# xfs_growfs /
-# xfs_growfs /var
-
-
 #!/bin/bash
 #resize disk from 20GB to 50GB
 growpart /dev/nvme0n1 4

5328fb8ecef5a6ed6a1aa0f00f7e2ebb8d9dc391
kumar <obulesu.1406@gmail.com>
2025-04-11 12:28:29 -0400

    correcte VPC module git source path

--- expense-Infra/00-vpc/vpc.tf
+++ expense-Infra/00-vpc/vpc.tf
@@ -1,11 +1,11 @@
 module "vpc" {
-    source = "git::https://github.com/obulesu2506/terraform-new.git?ref=terraform-aws-vpc/main"
+    source = "git::https://github.com/obulesu2506/terraform-new.git//terraform-aws-vpc?ref=main"
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
     private_subnet_cidrs = var.private_subnet_cidrs
-    database_subnet_cidrs = var.database_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrsS
     is_peering_required = var.is_peering_required
 }
